232c3aa913abe29c96aca5927f49b5a117ac589e
Belka-S <111767376+Belka-S@users.noreply.github.com>
2023-05-29 22:54:06 +0000

    Deploying to gh-pages from @ Belka-S/goit-react-hw-08-phonebook@d6fd012f95db78612d129d7a5b53f6fc88144969 🚀

--- src/components/App/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import { useAuth } from 'hooks/useAuth';
-import { Navigate } from 'react-router-dom';
-
-export const PrivateRote = ({ component, redirectTo = '/' }) => {
-  const { isLoggedIn, isRefreshing } = useAuth();
-  const shouldRedirect = !isLoggedIn && !isRefreshing;
-
-  return shouldRedirect ? <Navigate to={redirectTo} /> : component;
-};
--- src/pages/Home.jsx
+++
@@ -1,3 +0,0 @@
-import { Section } from 'components/Section/Section';
-
-export const Home = () => <Section mainTitle="Welcome!" />;
